b18ac49d44f419cd699818140c5aa5032129baf5
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2020-09-19 19:04:45 +0500

    Create basic project structure

---
+++ hpp/linear_algebra.hpp
@@ -0,0 +1,8 @@
+#ifndef LAB2_LINEAR_ALGEBRA_HPP
+#define LAB2_LINEAR_ALGEBRA_HPP
+
+#include <utility>
+
+std::pair<double**, double **> LUDecompose(double **);
+
+#endif //LAB2_LINEAR_ALGEBRA_HPP
---
+++ src/linear_algebra.cpp
@@ -0,0 +1,5 @@
+#include "../hpp/linear_algebra.hpp"
+
+std::pair<double **, double **> LUDecompose(double **matrix) {
+    return std::pair<double **, double **>();
+}
