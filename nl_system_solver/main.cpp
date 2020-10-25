bef34bdd3295fd54d5826d6b9f3c463db1b708a2
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2020-10-25 19:04:42 +0500

    Start implementing non-linear system solver

---
+++ nl_system_solver/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // Read the equation system to solve
+
+    // Solve using simple iteration method
+
+    // Solve using Newton's method
+
+    // Solve using gradient descent
+
+    return 0;
+}
